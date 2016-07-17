1d514781183d38725a79c05b2215c3cc1509d452
koopero <koopero@gmail.com>
2016-07-17 16:08:03 -0700

    replace log with treebird

--- src/node/log.js
+++ src/node/log.js
@@ -8,6 +8,7 @@ loopinLog.options = require('boptions')({
 
 const _ = require('lodash')
     , chalk = require('chalk')
+    , treebird = require('treebird')
 
 const event = require('../core/event')
 
@@ -36,8 +37,8 @@ function loopinLog() {
   function logSection ( name ) {
     write('\n')
 
-    write( style('delim', '--------------- '))
-    write( style('section', name||'' ))
+    write( '--------------- ')
+    write( name||'' )
     write('\n')
   }
 
@@ -48,64 +49,9 @@ function loopinLog() {
     var type = event.type
       , path = event.path
       , data = event.data
-      , width = 100
-      , typeWidth = 16
 
-
-    width -= column( type, 'type', typeWidth )
-    width -= column( path+' ', 'path' )
-
-    if ( _.isObject( data ) ) {
-      var pairsLength = 0
-        , pairs = _.map( data, function ( val, key ) {
-            if ( _.isUndefined( val ) )
-              return
-            val = toStr( val )
-            pairsLength += val.length + 4
-            return style('key', key ) + style('delim', ': ') + style('value', val )
-          } )
-
-      if ( pairsLength < width ) {
-        column( pairs.join( style('delim', ', ' ) ) )
-      } else {
-        write("\n")
-        write(_.repeat( ' ', typeWidth ) )
-        write( pairs.join( "\n" + _.repeat( ' ', typeWidth ) ) )
-      }
-    } else if ( !_.isUndefined( data ) ) {
-      column( style( 'value', toStr( data ) ) )
-    }
-
-    write( "\n" )
+    treebird( data, type, path )
 
     return true
-  }
-
-  function toStr( val ) {
-    if ( _.isNumber( val ) ) {
-      return val.toFixed( 3 )
-    }
-
-    return JSON.stringify( val )
-  }
-
-  function column( data, style, width, padder ) {
-
-    if ( width ) {
-      padder = padder || _.padStart
-      data = padder( data, width - 1 ) + ' '
-    }
-
-    const length = data.length
-
-    data = style && styles[style] ? styles[style]( data ) : data
-
-    write( data )
-
-    return length
-  }
-
-  function style( style, data ) {
-    return styles[style] ? styles[style]( data ) : data
   }
 }
