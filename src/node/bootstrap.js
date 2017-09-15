5f6c1aa718f595c5b7ff29b27f83de365b3a5da3
koopero <koopero@gmail.com>
2017-09-15 16:17:30 -0700

    Redo bootstrap

--- src/node/bootstrap.js
+++ src/node/bootstrap.js
@@ -1,35 +1,14 @@
 module.exports = bootstrap
-bootstrap.options = require('boptions')( {
-
-})
-
-
-const Loopin = require('../core/loopin')
-
-const loopinNative = require('loopin-native')
-
 
 
 function bootstrap() {
-  const opt = bootstrap.options( arguments )
-
   const loopin = this
       , Promise = loopin.Promise
 
-  loopin.plugin('files')
+  loopin.plugin('hook')
+  loopin.bootstrap = bootstrap
 
-  // console.log( 'bootstrap', opt )
-  const nativeOpt = {
-    run: true,
-    runCwd: loopin.filesAbsolute()
+  function bootstrap() {
+    return loopin.hookAll('bootstrap')
   }
-
-  return loopinNative( nativeOpt )
-    .then( function ( build ) {
-      loopin.plugin( 'stdio', build.process )
-      loopin.dispatchEvent( { path: 'native', type: 'open', data: { pid: build.process.pid } } )
-      loopin.emit( 'open' )
-
-      return loopin
-    })
 }
